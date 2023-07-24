f72cb43032820d27c56896c34658d1a3b7ba592e
YuanSeen <YuanSeen@foxmail.com>
2023-07-24 11:16:18 +0800

    进一步完善开发环境

--- src/main/java/io/github/YuanSeen/Main.java
+++ src/main/java/io/github/YuanSeen/Main.java
@@ -1,4 +1,13 @@
 package io.github.YuanSeen;
 
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Main.MOD_ID)
 public class Main {
+
+    public static final String MOD_ID = "modstu";
+    //修改模组名称
+
+
+
 }
