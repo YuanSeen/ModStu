616da4b54a8cfb8c3f7ad37929656e7711cd66ae
YuanSeen <YuanSeen@foxmail.com>
2023-07-26 16:09:40 +0800

    写了一个测试物品！！！！

--- src/main/java/io/github/YuanSeen/Main.java
+++ src/main/java/io/github/YuanSeen/Main.java
@@ -2,9 +2,6 @@ package io.github.YuanSeen;
 
 import com.mojang.logging.LogUtils;
 import io.github.YuanSeen.item.ModItem;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,7 +9,6 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 @Mod(Main.MOD_ID)
