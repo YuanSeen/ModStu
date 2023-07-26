a49683bc49bbf1780528680fe1ded5b27482e8a2
YuanSeen <YuanSeen@foxmail.com>
2023-07-26 15:34:06 +0800

    基本推倒重来了

--- src/main/java/io/github/YuanSeen/Main.java
+++ src/main/java/io/github/YuanSeen/Main.java
@@ -1,10 +1,10 @@
 package io.github.YuanSeen;
 
 import com.mojang.logging.LogUtils;
-import io.github.YuanSeen.init.BlockInit;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -18,23 +18,16 @@ import org.slf4j.Logger;
 public class Main {
 
     public static final String MOD_ID = "modstu";
-    //修改模组名称
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    //物品栏声明
-    public static final CreativeModeTab TUTORIAL_TAB = new CreativeModeTab(MOD_ID) {
-        @Override
-        public ItemStack makeIcon() {
-            //这个是物品栏的图标使用哪个物品来展示
-            return new ItemStack(BlockInit.EXAMPLE_BLOCK.get());
-        }
-    };
-
+    //重要的构造方法
     public Main(){
-        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-        // Register the commonSetup method for modloading
-        bus.addListener(this::commonSetup);
-        bus.addListener(this::setup);
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::setup);
 
     }
     private void setup(final FMLCommonSetupEvent event)
@@ -44,12 +37,8 @@ public class Main {
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
-        // Some common setup code
-        LOGGER.info("HELLO FROM COMMON SETUP");
-        LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
     public static class ClientModEvents
     {
