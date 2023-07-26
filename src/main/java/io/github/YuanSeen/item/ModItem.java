6627218d5121be8ccdebf4176220f685edbe5502
YuanSeen <YuanSeen@foxmail.com>
2023-07-26 15:59:42 +0800

    写了一个测试物品！！！！

--- src/main/java/io/github/YuanSeen/item/ModItem.java
+++ src/main/java/io/github/YuanSeen/item/ModItem.java
@@ -1,14 +1,18 @@
 package io.github.YuanSeen.item;
 
 import io.github.YuanSeen.Main;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.ForgeRegistry;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModItem {
     public static final DeferredRegister<Item> ITEM = DeferredRegister.create(ForgeRegistries.ITEMS, Main.MOD_ID);
+
+    public static final RegistryObject<Item> CE4SHI4WU4PIN3 = ITEM.register("ce4shi4wu4pin3",
+            ()->new Item(new Item.Properties().tab(CreativeModeTab.TAB_FOOD)));
 
     public static void register(IEventBus modEventBus){
         ITEM.register(modEventBus);
