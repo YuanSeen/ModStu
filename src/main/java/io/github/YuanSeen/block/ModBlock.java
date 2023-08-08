0db8a872fb22e826a387478e11cd15d8cc1b113e
YuanSeen <YuanSeen@foxmail.com>
2023-08-08 22:45:01 +0800

    写方块

--- src/main/java/io/github/YuanSeen/block/ModBlock.java
+++ src/main/java/io/github/YuanSeen/block/ModBlock.java
@@ -1,17 +1,34 @@
 package io.github.YuanSeen.block;
 
+import com.mojang.blaze3d.shaders.Uniform;
 import io.github.YuanSeen.Main;
+import io.github.YuanSeen.item.ModCreativeModeTab;
+import io.github.YuanSeen.item.ModItem;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 
 public class ModBlock {
 
     public static final DeferredRegister<Block> BLOCK = DeferredRegister.create(ForgeRegistries.BLOCKS, Main.MOD_ID);
     //创建方块注册者
 
-
+    public static final RegistryObject<Block> LING2SHI2KUANG4 = BLOCK.register("ling2shi2kuang4",
+            ()->new DropExperienceBlock(BlockBehaviour.Properties
+                    .of(Material.STONE)
+                    .strength(6f)
+                    .requiresCorrectToolForDrops()
+            ));
+    public static final RegistryObject<Item> LINSHI2KUANG4 = ModItem.ITEM.register("LIN2SHI2kuang4",
+            ()->new BlockItem(LING2SHI2KUANG4.get(),new Item.Properties().tab(ModCreativeModeTab.MODSTU_TAB_QI2TA1)));
 
     public static void register(IEventBus modEventBus){
         BLOCK.register(modEventBus);
