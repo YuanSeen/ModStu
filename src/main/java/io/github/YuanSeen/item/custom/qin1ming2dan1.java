e5d0d1efb071454e70ac03eb6b52026fec72e0f2
YuanSeen <YuanSeen@foxmail.com>
2023-09-22 11:34:13 +0800

    添加了一个加buff的功能

--- src/main/java/io/github/YuanSeen/item/custom/qin1ming2dan1.java
+++ src/main/java/io/github/YuanSeen/item/custom/qin1ming2dan1.java
@@ -4,6 +4,8 @@ package io.github.YuanSeen.item.custom;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -19,6 +21,8 @@ public class qin1ming2dan1 extends Item {
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand hand) {
         if(!level.isClientSide() && hand.equals(InteractionHand.MAIN_HAND)){
             player.sendSystemMessage(Component.literal("一股神秘力量阻止你服下丹药。"));
+            player.sendSystemMessage(Component.literal("但药香让你感受到满足。"));
+            player.addEffect(new MobEffectInstance(MobEffects.SATURATION,20));
         }
         return super.use(level, player, hand);
     }
