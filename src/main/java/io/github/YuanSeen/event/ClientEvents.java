d111d800866a05786c5e583f2d5980141da6975a
YuanSeen <YuanSeen@foxmail.com>
2023-10-19 09:18:25 +0800

    绑定了一个按键O
    叫做静心咒，可以消除自身的buff（目前只写了消除幸运效果）

---
+++ src/main/java/io/github/YuanSeen/event/ClientEvents.java
@@ -0,0 +1,33 @@
+package io.github.YuanSeen.event;
+
+import io.github.YuanSeen.Main;
+import io.github.YuanSeen.util.KeyBinding;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+public class ClientEvents {
+    @Mod.EventBusSubscriber(modid = Main.MOD_ID,value = Dist.CLIENT)
+    public static class ClientForgeEvent{
+        @SubscribeEvent
+        public static void onKeyInput(InputEvent.Key event){
+            if (KeyBinding.JING4XI1SHU4_KEY.consumeClick()){
+                Minecraft.getInstance().player.removeEffect(MobEffects.LUCK);
+            }
+
+        }
+
+    }
+    @Mod.EventBusSubscriber(modid = Main.MOD_ID,value = Dist.CLIENT,bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvents{
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event){
+            event.register(KeyBinding.JING4XI1SHU4_KEY);
+        }
+
+    }
+}
---
+++ src/main/java/io/github/YuanSeen/util/KeyBinding.java
@@ -0,0 +1,18 @@
+package io.github.YuanSeen.util;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBinding {
+    public static final String AN4JIAN4 = "key.category.modstu.an4jian4";
+    public static final String KEY_NING2XIN1ZHOU4 =  "key.modstu.ning2xin1zhou4";
+
+    public static final KeyMapping JING4XI1SHU4_KEY = new KeyMapping(
+            KEY_NING2XIN1ZHOU4,
+            KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_O,
+            AN4JIAN4);
+}
